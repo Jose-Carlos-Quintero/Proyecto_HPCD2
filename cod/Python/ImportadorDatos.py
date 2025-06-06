a07b00f912612b244965fd7e7a4284c78c5f3abc
José Carlos Quintero <jose.quinterocedeno@ucr.ac.cr>
2025-06-06 14:53:53 -0600

    fix: arreglar documentación en módulo de importación de datos

--- cod/Python/ImportadorDatos.py
+++ cod/Python/ImportadorDatos.py
@@ -14,6 +14,10 @@ class ImportadorDatos:
         ----------
         ruta_archivo : str
             Ruta al archivo SPSS (.sav) que se desea importar.
+            
+        Retorna
+        -------
+        None
         """
         self.__ruta = ruta_archivo
         self.__datos = None
@@ -23,53 +27,111 @@ class ImportadorDatos:
 
     @property
     def datos(self):
-        """Accede al DataFrame de datos importados."""
+        """Accede al DataFrame de datos importados.
+        Parámetros
+        ----------
+        ruta_archivo : str
+            Ruta al archivo SPSS (.sav) que se desea importar.
+            
+        Retorna
+        -------
+        pd.DataFrame
+            dataframe con los datos
+        """
         return self.__datos
 
     @datos.setter
     def datos(self, nuevo_df):
-        """Asigna un nuevo DataFrame a los datos importados."""
+        """Asigna un nuevo DataFrame a los datos importados.
+        
+        Parámetros
+        ----------
+        nuevo_df : pd.DataFrame
+            Nuevo dataframe que se quiere importar
+            
+        Retorna
+        -------
+        None
+        """
         self.__datos = nuevo_df
 
     @property
     def etiquetas(self):
-        """Accede al diccionario de etiquetas importadas desde el .sav."""
+        """Accede al diccionario de etiquetas importadas desde el .sav.
+        
+        Parámetros
+        ----------
+        None
+            
+        Retorna
+        -------
+        pyreadstat.MetadataContainer
+            Contenedor de datos del dataframe
+        """
         return self.__etiquetas
 
     @etiquetas.setter
     def etiquetas(self, nuevas_etiquetas):
-        """Asigna nuevas etiquetas."""
+        """Asigna nuevas etiquetas.
+        
+        Parámetros
+        ----------
+        nuevas_etiquetas: pyreadstat.MetadataContainer
+            nuevas etiquetas del df
+            
+        Retorna
+        -------
+        None
+        
+        """
         self.__etiquetas = nuevas_etiquetas
 
     @property
     def ruta(self):
-        """Accede a la ruta del archivo importado."""
+        """Accede a la ruta del archivo importado.
+        
+        Parámetros
+        ----------
+        None
+            
+        Retorna
+        -------
+        str
+            ruta del archivo de la base de datos
+        
+        """
         return self.__ruta
 
     @ruta.setter
     def ruta(self, nueva_ruta: str):
-        """Asigna una nueva ruta y recarga los datos."""
+        """Asigna una nueva ruta y recarga los datos.
+        
+        Parámetros
+        ----------
+        nueva_ruta: str
+            nueva ruta al archivo de la base de datos
+            
+        Retorna
+        -------
+        None
+        """
         self.__ruta = nueva_ruta
         self.__datos, self.__etiquetas = pyreadstat.read_sav(self.__ruta)
 
     def __str__(self):
         """
         Retorna una representación textual resumida del objeto.
-
+        
+        Parámetros
+        ----------
+        None
+            
         Retorna
         -------
         str
         """
         return (f"ImportadorDatos desde '{self.__ruta}' con {self.__datos.shape[0]} filas "
                 f"y {self.__datos.shape[1]} columnas.")
-
-    def resumen_general(self):
-        """Imprime dimensiones, tipos de variables y primeras filas del DataFrame."""
-        print("Dimensiones:", self.__datos.shape)
-        print("\nTipos de variables:")
-        print(self.__datos.dtypes.value_counts())
-        print("\nPrimeras filas:")
-        print(self.__datos.head())
 
     def seleccionar_variables(self, lista_vars: list):
         """
@@ -79,13 +141,29 @@ class ImportadorDatos:
         ----------
         lista_vars : list
             Lista de nombres de variables a conservar.
+            
+        Retorna
+        -------
+        pd.DataFrame
+            dataframe con las variables seleccionadas
         """
         self.__datos = self.__datos[lista_vars]
+        
+        return self.__datos
 
     def factorizar_todo(self):
         """
         Reemplaza los códigos numéricos por sus etiquetas y convierte las columnas a tipo categoría
         para todas las variables que tengan etiquetas definidas en el archivo .sav.
+        
+        Parámetros
+        ----------
+        None
+            
+        Retorna
+        -------
+        pd.DataFrame
+            Dataframe con las variables factorizadas
         """
         etiquetas = self.__etiquetas.variable_value_labels
 
@@ -93,6 +171,7 @@ class ImportadorDatos:
             if col in etiquetas:
                 self.__datos[col] = self.__datos[col].replace(etiquetas[col])
                 self.__datos[col] = self.__datos[col].astype("category")
+        return self.__datos 
                 
     def eliminar_na_columnas(self, columnas = None):
         """
@@ -102,14 +181,25 @@ class ImportadorDatos:
         ----------
         columnas : list
             Lista de nombres de columnas en las cuales se eliminarán filas con valores NA.
+            
+        Retorna
+        -------
+        pd.DataFrame
+            Dataframe con las observaciones NA filtradas
         """
         if columnas is None:
             columnas = self.__datos.columns
         self.__datos = self.__datos.dropna(subset=columnas)
         
+        return self.__datos
+        
     def contar_na_por_columna(self):
         """
         Retorna un Series con la cantidad de valores NA por cada columna del DataFrame.
+        
+        Parámetros
+        ----------
+        None
 
         Retorna
         -------
